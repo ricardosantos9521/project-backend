2a976b67b9171d2fe4e2ffc442e0123e64a88615
ricardo <ricardo.santos9521@outlook.com>
2019-06-21 20:03:58 +0100

    Revert "https redirect"
    This reverts commit ce8c62cf202ef61365225d653466c58cfacdfed5.

--- src/Startup.cs
+++ src/Startup.cs
@@ -101,7 +101,6 @@ namespace backendProject
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-                app.UseHttpsRedirection();
             }
 
             app.UseCors(builder =>
